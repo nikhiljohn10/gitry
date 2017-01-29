2fc93f3c05819278b56e10b67be76248a09275df
Nikhil John <nikhiljohn1010@gmail.com>
2017-01-29 13:55:53 +0530

    Fix 3

--- install.sh
+++ install.sh
@@ -39,17 +39,20 @@ echo "SSH is installed and running on your system."
 echo "Adding git user to your system"
 adduser git
 echo "Git user successfully added"
-su $SUDO_USER -c "./configure_client.sh ${email}"
-mkdir /home/git/.bin
+su $SUDO_USER -c "./configure_client.sh ${email}"	# Executing client config file
+mkdir /home/git/.bin 
 cp ./configure.sh /home/git/.bin/configure
 chmod +x /home/git/.bin/configure
-chown git:git /home/git/.bin/configure
-su git -c '/home/git/.bin/configure'
-cat /home/$SUDO_USER/.ssh/id_rsa.pub >> /home/git/.ssh/authorized_keys
+chown git:git /home/git/.bin/configure 				
+su git -c '/home/git/.bin/configure' 				# Executing server config file
+cat /home/$SUDO_USER/.ssh/id_rsa.pub >> /home/git/.ssh/authorized_keys # Adding public key to server
 cp ./gitry /home/git/.bin/gitry
 chown git:git /home/git/.bin/gitry
 chmod +x /home/git/.bin/gitry
-ln -s /home/git/.bin/gitry /usr/local/bin/gitry
+ln -s /home/git/.bin/gitry /usr/local/bin/gitry 	# Create a symbolic link to bin directory where executables exists
+cp ./git /var/lib/AccountsService/users/git 		# Diable GUI login
+chown root:root /var/lib/AccountsService/users/git
+rm /home/git/.bin/configure 						# Removing server config file
 echo "Gitry installed successfully"
 echo "To clone a repo: git clone git@localhost:/home/git/repo_name.git"
 echo "To add remote: git remote add origin git@localhost:/home/git/repo_name.git"
